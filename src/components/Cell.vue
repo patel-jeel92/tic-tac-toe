de16ed934799214244a7065f95de17a45504312c
Jeel Patel <jeel@Jeels-MacBook-Pro.local>
2021-02-22 17:07:43 -0600

    Initial implementation of cell component

---
+++ src/components/Cell.vue
@@ -0,0 +1,33 @@
+<template>
+  <button class="button" @click="handleClick">{{ value }}</button>
+</template>
+
+<script>
+export default {
+  name: "Cell",
+  data: () => {
+    return {
+      value: "X",
+      frozen: Boolean
+    };
+  },
+  props: {
+    index: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    handleClick() {
+      // TODO: Handle click event. Use store action to update cell map with active Player and update value.
+    }
+  }
+};
+</script>
+
+<style>
+.button {
+  width: 64px;
+  height: 64px;
+}
+</style>
