6e524a304c5e6c16b9aa5dfca678580964bef14c
Jeel Patel <jeel@Jeels-MacBook-Pro.local>
2021-02-22 17:09:42 -0600

    Initial implementation of Board component

---
+++ src/components/Board.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div>
+      <div class="board-row">
+        <Cell index="1" @selection="onCellSelection"></Cell>
+        <Cell index="2" @selection="onCellSelection"></Cell>
+        <Cell index="3" @selection="onCellSelection"></Cell>
+      </div>
+      <div class="board-row">
+        <Cell index="4" @selection="onCellSelection"></Cell>
+        <Cell index="5" @selection="onCellSelection"></Cell>
+        <Cell index="6" @selection="onCellSelection"></Cell>
+      </div>
+      <div class="board-row">
+        <Cell index="7" @selection="onCellSelection"></Cell>
+        <Cell index="8" @selection="onCellSelection"></Cell>
+        <Cell index="9" @selection="onCellSelection"></Cell>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Cell from "../components/Cell";
+export default {
+  name: "Board",
+  components: {
+    Cell
+  },
+  data: () => {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style></style>
